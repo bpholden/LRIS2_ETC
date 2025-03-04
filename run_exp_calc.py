10666dc3500a3a17718d42a6d67461ff876aa877
Bradford Holden <brad.p.holden@gmail.com>
2025-03-04 17:25:11 -0500

    run main

--- run_exp_calc.py
+++ run_exp_calc.py
@@ -1,6 +1,8 @@
 '''Run the exposure time calculator'''
 import argparse
 
+import matplotlib.pyplot as plt
+
 import ExpCalc
 import Instrument
 import Telescope
@@ -25,9 +27,7 @@ def parse_args():
                         help='Template file')
     return parser.parse_args()
 
-def main():
-
-    args = parse_args()
+def main(args):
 
     keck_1 = Telescope.Telescope()
     keck_1.keckone()
@@ -35,3 +35,22 @@ def main():
     lris2_blue.lris2_blue(grating=args.blue_grism)
     lris2_red = Instrument.Instrument()
     lris2_red.lris2_red(grating=args.red_grism)
+
+    blue_exp_calc = ExpCalc.ExpCalc(args.mag, args.filter, args.seeing, args.airmass, \
+                                    args.redshift, args.template, lris2_blue, keck_1)
+    red_exp_calc = ExpCalc.ExpCalc(args.mag, args.filter, args.seeing, args.airmass, \
+                                   args.redshift, args.template, lris2_red, keck_1)
+
+    blue_snr = blue_exp_calc.compute_spectrum(args.time, args.slit_length, args.slit_width)
+    red_snr = red_exp_calc.compute_spectrum(args.time, args.slit_length, args.slit_width)
+
+    plt.plot(blue_exp_calc.wave, blue_snr, label='Blue')
+    plt.plot(red_exp_calc.wave, red_snr, label='Red')
+    plt.xlabel('Wavelength (Angstroms)')
+    plt.ylabel('SNR')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
